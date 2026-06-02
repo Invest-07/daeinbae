c293e957c75969e45c63db9e23e7cffdc7a9c922
Invest-07 <ferraribuyingchallenge@gmail.com>
2026-06-02 02:21:37 +0000

    1

--- src/App.jsx
+++ src/App.jsx
@@ -3,19 +3,18 @@ import './App.css'
 function App() {
   return (
     <div className="page">
-      <img src="./Star.png" className="star star-left" alt="star" />
-      <div className="center">
-        <div className="logo">Owl Rock</div>
-        <div className="menu-grid">
-          <div className="menu-item">Dashboard</div>
-          <div className="menu-item">Portfolio</div>
-          <div className="menu-item">Strategy</div>
-          <div className="menu-item">Invest</div>
-          <div className="menu-item">About</div>
-          <div className="menu-item">Chat</div>
-        </div>
+      <div className="stars-bg">
+        <img src="./Star.png" className="star" alt="star" />
+        <img src="./Star.png" className="star" alt="star" />
       </div>
-      <img src="./Star.png" className="star star-right" alt="star" />
+      <div className="menu-grid">
+        <div className="menu-item">Company</div>
+        <div className="menu-item">Portfolio</div>
+        <div className="menu-item">Strategy</div>
+        <div className="menu-item">Invest</div>
+        <div className="menu-item">About</div>
+        <div className="menu-item">Contact</div>
+      </div>
     </div>
   )
 }
