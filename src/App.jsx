da9651bb4097084cf3eca03d3f441c656117e6d9
Invest-07 <ferraribuyingchallenge@gmail.com>
2026-06-02 02:15:00 +0000

    1

---
+++ src/App.jsx
@@ -0,0 +1,23 @@
+import './App.css'
+
+function App() {
+  return (
+    <div className="page">
+      <img src="/daeinbae/star.png" className="star star-left" alt="star" />
+      <div className="center">
+        <div className="logo">Owl Rock</div>
+        <div className="menu-grid">
+          <div className="menu-item">Dashboard</div>
+          <div className="menu-item">Portfolio</div>
+          <div className="menu-item">Strategy</div>
+          <div className="menu-item">Invest</div>
+          <div className="menu-item">About</div>
+          <div className="menu-item">Chat</div>
+        </div>
+      </div>
+      <img src="/daeinbae/star.png" className="star star-right" alt="star" />
+    </div>
+  )
+}
+
+export default App
